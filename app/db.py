dac1a7dc6c99111704fa1cd4ef2185effbcc10ba
sr8e <sr@mell0w-5phere.net>
2024-02-02 08:33:07 +0900

    fix/add db utils

--- app/db.py
+++ app/db.py
@@ -8,6 +8,7 @@ from flask import current_app, g
 def get_db():
     if "db" not in g:
         g.db = sqlite3.connect(current_app.config["DATABASE"])
+        g.db.row_factory = sqlite3.Row
 
     return g.db
 
@@ -25,3 +26,18 @@ def create_db():
     with current_app.open_resource("schema.sql") as f:
         db.executescript(f.read().decode("utf-8"))
     click.echo("created db.")
+
+
+def get_users_methods(con, uid, include_default=False):
+    query = "select * from `method` where `user_id` = ?"
+    if include_default:
+        query += "or `user_id` is null"
+    res = con.execute(query, (uid,))
+    return res.fetchall()
+
+def get_users_genres(con, uid, include_default=False):
+    query = "select * from `genre` where `user_id` = ?"
+    if include_default:
+        query += "or `user_id` is null"
+    res = con.execute(query, (uid,))
+    return res.fetchall()
--- app/schema.sql
+++ app/schema.sql
@@ -7,14 +7,14 @@ create table user(
 create table genre(
     id integer primary key autoincrement,
     name text not null,
-    user_id integer references user (id) on delete cascade
+    user_id text references user (id) on delete cascade
 );
 
 create table method(
     id integer primary key autoincrement,
     name text not null,
     not_own integer not null default 0,
-    user_id integer references user (id) on delete cascade
+    user_id text references user (id) on delete cascade
 );
 
 create table payment(
@@ -28,5 +28,5 @@ create table payment(
     method integer references method (id) on delete set null
 );
 
-insert into genre("name") values ("食費"), ("日用品"), ("交通費"), ("趣味・娯楽");
+insert into genre("name") values ("食費"), ("日用品"), ("交通費"), ("趣味・娯楽"), ("光熱費");
 insert into method("name") values ("現金");
